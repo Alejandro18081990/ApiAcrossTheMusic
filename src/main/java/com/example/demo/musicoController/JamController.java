b3bc6c5e03139cc7e9eb40c33f8ca5d2fc2740af
manuel.nunez <manuel.nunez@alten.es>
2024-04-10 19:14:38 +0200

    Docs: - Implemento Swagger para documentar la api y facilitar su lectura cuando desarrolle el frontend.

--- src/main/java/com/example/demo/musicoController/JamController.java
+++ src/main/java/com/example/demo/musicoController/JamController.java
@@ -3,6 +3,7 @@ package com.example.demo.musicoController;
 import com.example.demo.entities.JamSession;
 import com.example.demo.services.jamsServices.JamService;
 import com.example.demo.services.jamsServices.JamServiceImpl;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +16,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping("apiATM/jams")
 @RequiredArgsConstructor
+@Tag(name = "PI Across the music - Controlador Jams")
 public class JamController {
 
     @Autowired
--- src/main/java/com/example/demo/musicoController/MusicoController.java
+++ src/main/java/com/example/demo/musicoController/MusicoController.java
@@ -3,6 +3,8 @@ package com.example.demo.musicoController;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,49 +28,51 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("apiATM/musicos")
+@Tag(name = "PI: Across the music - Controlador Musicos")
 public class MusicoController {
 
-	@Autowired
-	private final MusicoRepository musicoRepositorio;
-	@Autowired
-	MusicoDto musicosDTO;
-	@Autowired
-	private MusicoDtoConverter musicoDtoConverter;
-	@Autowired
-	private JamSessionRepository jamSessionRepository;
-	@Autowired
-	private EstiloRepository estiloRepository;
-	@Autowired
-	private MusicoServiceImpl musicoServiceImpl;
+    @Autowired
+    private final MusicoRepository musicoRepositorio;
+    @Autowired
+    MusicoDto musicosDTO;
+    @Autowired
+    private MusicoDtoConverter musicoDtoConverter;
+    @Autowired
+    private JamSessionRepository jamSessionRepository;
+    @Autowired
+    private EstiloRepository estiloRepository;
+    @Autowired
+    private MusicoServiceImpl musicoServiceImpl;
 
-	@PostMapping("/musico")
-	public ResponseEntity<?> createMusico(@RequestBody Musico musicoNuevo) {
-		Musico newMusician = musicoServiceImpl.save(musicoNuevo);
-		System.out.println(musicoNuevo);
-		return ResponseEntity.status(HttpStatus.CREATED).body(newMusician);
-	}
+    @Operation(summary = "Añade un músico a la tabla Musico")
+    @PostMapping("/musico")
+    public ResponseEntity<?> createMusico(@RequestBody Musico musicoNuevo) {
+        Musico newMusician = musicoServiceImpl.save(musicoNuevo);
+        System.out.println(musicoNuevo);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newMusician);
+    }
+    @Operation(summary = "Devuelve todos los musicos de la tabla Musico")
+    @GetMapping("/")
+    public ResponseEntity<?> getAllMusicos() {
+        Iterable<Musico> listadoMusicos = musicoServiceImpl.findAll();
+        System.out.println("----------------------------------------------------------------------" + listadoMusicos);
+        if (listadoMusicos == null)
+            ResponseEntity.notFound().build();
+        return ResponseEntity.ok(listadoMusicos);
+    }
+    @Operation(summary = "Devuelve todos los musicos que tocan un instrumento y estilos determinados")
+    @GetMapping("/{nombreInstrumento}/{nombreEstilo}")
+    public ResponseEntity<?> getMusicoByEstiloAndInstrumento(@PathVariable String nombreInstrumento,
+                                                             @PathVariable String nombreEstilo) {
+        List<Musico> musicos = musicoServiceImpl.findByEstiloAndInstrumento(nombreInstrumento, nombreEstilo);
 
-	@GetMapping("/")
-	public ResponseEntity<?> getAllMusicos() {
-		Iterable<Musico> listadoMusicos = musicoServiceImpl.findAll();
-		System.out.println("----------------------------------------------------------------------" + listadoMusicos);
-		if (listadoMusicos == null)
-			ResponseEntity.notFound().build();
-		return ResponseEntity.ok(listadoMusicos);
-	}
-
-	@GetMapping("/{nombreInstrumento}/{nombreEstilo}")
-	public ResponseEntity<?> getMusicoByEstiloAndInstrumento(@PathVariable String nombreInstrumento,
-			@PathVariable String nombreEstilo) {
-		List<Musico> musicos = musicoServiceImpl.findByEstiloAndInstrumento(nombreInstrumento, nombreEstilo);
-
-		if (musicos == null) {
-			ResponseEntity.notFound().build();
-			System.out.println("Error en la solicitud");
-		}
-		List<MusicoDto> musicosConsultaDto = musicos.parallelStream().map(musicoDtoConverter::convertirADTO)
-				.collect(Collectors.toList());
-		System.out.println("ConsultaMusicosByInstrumentoByEstilo " + musicos);
-		return ResponseEntity.ok(musicosConsultaDto);
-	}
+        if (musicos == null) {
+            ResponseEntity.notFound().build();
+            System.out.println("Error en la solicitud");
+        }
+        List<MusicoDto> musicosConsultaDto = musicos.parallelStream().map(musicoDtoConverter::convertirADTO)
+                .collect(Collectors.toList());
+        System.out.println("ConsultaMusicosByInstrumentoByEstilo " + musicos);
+        return ResponseEntity.ok(musicosConsultaDto);
+    }
 }
